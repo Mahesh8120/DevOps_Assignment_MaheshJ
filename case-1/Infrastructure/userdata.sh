80e24db6473e97a464f0bbb633375780c302fd14
Mahesh8120 <jonnalagadamahi@gmail.com>
2026-04-08 21:34:03 +0530

    devops-challenge

--- case-1/Infrastructure/userdata.sh
+++ case-1/Infrastructure/userdata.sh
@@ -4,91 +4,40 @@ set -e
 # =========================
 # DISK EXPANSION (OPTIONAL)
 # =========================
-dnf install -y cloud-utils-growpart xfsprogs lvm2
-
-growpart /dev/nvme0n1 4 || true
-lvextend -L +30G /dev/mapper/RootVG-homeVol || true
-xfs_growfs /home || true
-
-
-# =========================
-# SYSTEM UPDATE
-# =========================
-dnf update -y
-
-
-# =========================
-# INSTALL JAVA (Jenkins dependency)
-# =========================
-dnf install -y java-17
-
+sudo growpart /dev/nvme0n1 4
+sudo pvresize /dev/nvme0n1p4
+sudo lvextend -L +20G /dev/mapper/RootVG-varVol
+sudo xfs_growfs /var
 
 # =========================
 # INSTALL JENKINS
 # =========================
-dnf install -y wget
-
-wget -O /etc/yum.repos.d/jenkins.repo \
-https://pkg.jenkins.io/redhat-stable/jenkins.repo
-
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-
-dnf install -y jenkins
-
-systemctl enable jenkins
-systemctl start jenkins
-
+sudo curl -o /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-21-openjdk -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
 
 # =========================
 # INSTALL DOCKER
 # =========================
-dnf install -y docker
-
-systemctl enable docker
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
-
-# Add Jenkins + ec2-user to docker group
-usermod -aG docker jenkins
+systemctl enable docker
 usermod -aG docker ec2-user
 
 
-# =========================
-# INSTALL GIT
-# =========================
-dnf install -y git
 
 
-# =========================
-# INSTALL UNZIP (required for AWS CLI)
-# =========================
-dnf install -y unzip curl
 
 
-# =========================
-# INSTALL AWS CLI v2
-# =========================
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o awscliv2.zip
-
-unzip awscliv2.zip
-./aws/install
-
-rm -rf aws awscliv2.zip
 
 
-# =========================
-# ENABLE SERVICES
-# =========================
-systemctl restart jenkins
-systemctl restart docker
 
 
-# =========================
-# FINAL STATUS CHECK (LOG)
-# =========================
-echo "Jenkins status:"
-systemctl status jenkins --no-pager
 
-echo "Docker status:"
-systemctl status docker --no-pager
 
-echo "Setup completed successfully"
